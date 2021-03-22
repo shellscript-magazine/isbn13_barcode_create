89987f01296b3a9073ddb44c44ab0ddc79a8514d
Shell Script Magazine <35516434+shellscript-magazine@users.noreply.github.com>
2021-03-22 16:24:42 +0900

    Add files via upload

---
+++ barcode_create.py
@@ -0,0 +1,17 @@
+# ISBN13 Barcode's create program for Linux OS
+# pip3 install python-barcode
+# $ python3 barcode_create.py [ISBN13] [Vol numbar(example:vol68)]
+# Two files is created, SVG and Png. 
+import barcode
+from barcode.writer import ImageWriter
+from barcode import generate
+import sys
+
+bar_code = sys.argv[1]
+file_name = sys.argv[2]
+code_format = 'isbn13'
+
+bar_code_class = barcode.get_barcode_class(code_format) 
+bar_code_make = bar_code_class(bar_code, writer=ImageWriter()) 
+bar_code_png = bar_code_make.save(file_name) 
+bar_code_svg = generate(code_format, bar_code, output=file_name)
